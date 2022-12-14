2922de81fd6b7b8a08258711bb3eba71fd694fe3
Madeline Bertrand <madelinebertrand17@gmail.com>
2022-12-14 18:24:19 +0100

    Initial commit

---
+++ src/main/java/com/madeline/madelineexo/MadelineexoApplication.java
@@ -0,0 +1,13 @@
+package com.madeline.madelineexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadelineexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadelineexoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madeline/madelineexo/MadelineexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madeline.madelineexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadelineexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
